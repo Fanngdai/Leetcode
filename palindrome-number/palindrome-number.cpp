ae736349a2313e2ddf976961a68ca4ed2cfe9c72
Fanng <17479928+Fanngdai@users.noreply.github.com>
2021-12-02 19:39:56 -0500

    Time: 12 ms (60.45%), Space: 5.8 MB (74.12%) - LeetHub

---
+++ palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+ 
+    bool check(int x, int &y){
+        if ( x == 0 ) return true;
+        if (check (x / 10,y) ){
+            if (x % 10 == y % 10){
+                y = y / 10;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        return check( x , x );
+    }
+};
