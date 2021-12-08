8d3c2b2de47c42feaa58bcbbdfdf7d9ba7790cde
Fanng <17479928+Fanngdai@users.noreply.github.com>
2021-12-07 19:09:32 -0500

    Time: 0 ms (100.00%), Space: 46.8 MB (44.20%) - LeetHub

--- count-complete-tree-nodes/count-complete-tree-nodes.java
+++ count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,10 +14,48 @@
  * }
  */
 class Solution {
-    public int countNodes(TreeNode root) {
+    public int countNodesFirstAttempt(TreeNode root) {
         if(root == null) {
             return 0;
         }
         return countNodes(root.left) + countNodes(root.right) + 1;
     }
+    
+    public int countNodes(TreeNode root) {
+        if(root == null)
+            return 0;
+        int treeHeight = getHeight(root);
+        return binSearch(root, treeHeight) + (int)Math.pow(2, treeHeight-1);
+    }
+    
+    /*
+     * one right then all left, should be the middle of the node
+     */
+    private int binSearch(TreeNode root, int height) {        
+        if(height==1) {
+            return 0;
+        } else {
+            int counter = height-1;
+            TreeNode temp = root.right;
+            while(counter-- != 1) {
+                temp = temp.left;
+            }
+            
+            if(temp == null) {
+                return binSearch(root.left, height-1);
+            } else {
+                return binSearch(root.right, height-1) + ((int)Math.pow(2,height-1))/2;
+            }
+        }
+    }
+    
+    private int getHeight(TreeNode root) {
+        if(root == null) {
+            return 0;
+        } else {
+            return getHeight(root.left) + 1;
+        }
+    }
+    
+    
 }
