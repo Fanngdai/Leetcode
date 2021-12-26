7c6cf127e9ead3ad4cd9e6f1626942502946d699
Fanny <17479928+Fanngdai@users.noreply.github.com>
2021-12-25 22:41:11 -0500

    Time: 2 ms (27.31%), Space: 41.8 MB (6.08%) - LeetHub

---
+++ maximum-product-subarray/maximum-product-subarray.java
@@ -0,0 +1,41 @@
+class Solution {
+    /*
+     * counter      product all the way until you reach 0
+     * counter2     product all the way until you reach 0, but starts after the first neg val
+     * max          max to be returned 
+     *
+     * If only one value, return that one value
+     */
+    public int maxProduct(int[] nums) {
+        int counter = nums[0], counter2 = nums[0], max = nums[0];
+        boolean zeroFound = true;
+        
+        for(int i=1; i<nums.length; i++) {            
+            // prev was a 0, so set this val to next.
+            if(counter == 0) {
+                counter = nums[i];
+                zeroFound = true;
+            } else {
+                // counter2 always equals the value after the first neg value
+                if(counter < 0 && zeroFound) {
+                    counter2 = 1;
+                    zeroFound = false;
+                }
+                counter *= nums[i];
+                counter2 *= nums[i];
+                max = Math.max(max, Math.max(counter, counter2));
+            }
+            max = Math.max(nums[i], max);
+        }
+        return max;
+    }
+}
+
+// [-2,-5,-2,-4,-3,-1,-2,-4,-6,-7,-5,-6,-7]
+// [2,3,-2,4]
+// [-2,0,-1]
+// [-2]
+// [0,2]
+// [2,-5,-2,-4,3]
+// [1,0,-1,2,3,-5,-2]
+// [0,2,3,4,6]
