7e0eb9d724f2b3b3fff28af7913246de17c8b9bd
Fanny <17479928+Fanngdai@users.noreply.github.com>
2021-12-29 01:27:27 -0500

    Time: 2 ms (14.08%), Space: 52.7 MB (8.88%) - LeetHub

---
+++ reverse-string/reverse-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0, end = s.length-1;
+        
+        while(start < end) {
+            swap(s, start, end);
+            start++;
+            end--;
+        }
+    }
+    
+    private void swap(char[] s, int a, int b) {
+        char temp = s[a];
+        s[a] = s[b];
+        s[b] = temp;
+    }
+}
