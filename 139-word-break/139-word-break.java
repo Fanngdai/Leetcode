af2a16fe92e7a847225f31938eb77ed7c84bdbcc
Fanny <17479928+Fanngdai@users.noreply.github.com>
2022-01-05 22:54:54 -0500

    Time: 3 ms (85.14%), Space: 39.1 MB (74.41%) - LeetHub

---
+++ 139-word-break/139-word-break.java
@@ -0,0 +1,68 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Trie trie = new Trie();
+        Set<Integer> wordCountSet = new HashSet<>();
+        
+        for(String word: wordDict) {
+            trie.insert(word);
+            wordCountSet.add(word.length());
+        }
+        
+        return wordBreakHelper(s, trie, wordCountSet, new HashSet<>());
+    }
+    
+    private boolean wordBreakHelper(String s, Trie trie, Set<Integer> wordCountSet, Set<String> wordSet) {
+        if(s.equals("")) return true;
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length(); i++) {
+            sb.append(s.charAt(i));
+            if(wordCountSet.contains(i+1) && !wordSet.contains(s.substring(i+1, s.length()))
+               && trie.exist(sb.toString())) {
+                wordSet.add(s.substring(i+1, s.length()));
+                if(wordBreakHelper(s.substring(i+1,s.length()), trie, wordCountSet, wordSet)) {
+                    return true;
+                }
+                // wordSet.remove(s.substring(i+1, s.length()));
+            }
+        }
+        return false;
+    }
+}
+
+class Trie {
+    Trie[] root = new Trie[26];
+    boolean end;
+    
+    public void insert(String word) {
+        Trie temp = this;
+        for(int i=0; i<word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if(temp.root[index] == null) {
+                temp.root[index] = new Trie();
+            }
+            temp = temp.root[index];
+        }
+        temp.end = true;
+    }
+    
+    public boolean exist(String word) {
+        Trie temp = this;
+        char c;
+        for(int i=0; i<word.length(); i++) {
+            c = word.charAt(i);
+            temp = temp.root[c-'a'];
+            if(temp == null) {
+                return false;
+            }
+        }
+        return temp.end;
+    }
+}
+
+// "bb"
+// ["a","b","bbb","bbbb"]
+// "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+// ["aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa","ba"]
+// "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
+// ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
