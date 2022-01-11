edd88e4c3d9c3540d64aed042ff95d734babe6ba
Fanny <17479928+Fanngdai@users.noreply.github.com>
2022-01-10 23:27:43 -0500

    Time: 0 ms (100.00%), Space: 36.4 MB (47.21%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,11 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
+        // return robRecursion(nums);
+        
         int len = nums.length;
         if(len == 1) {
             return nums[0];
         }
         
+        nums[1] = Math.max(nums[0],nums[1]);
+        for(int i=2; i<len; i++) {
+            nums[i] = Math.max(nums[i-2] + nums[i], nums[i-1]);
+        }
+        return Math.max(nums[len-2], nums[len-1]);
+    }
+    
+    private int robRecursion(int[] nums) {
+        int len = nums.length;
+        if(len == 1) {
+            return nums[0];
+        }
         int[] rtn = new int[len];
+        // O(n)
         Arrays.fill(rtn, -1);
         robHelper(nums, rtn, 0, 0);
         robHelper(nums, rtn, 0, 1);
