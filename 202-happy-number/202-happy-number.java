c87906f166ee69f38010379678983453d521284e
Fanny <17479928+Fanngdai@users.noreply.github.com>
2022-01-12 14:39:31 -0500

    Time: 1 ms (84.67%), Space: 38.1 MB (16.41%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean isHappy(int n) {
-        // int[] square = new int[]{0, 1, 4, 9, 16, 25, 36, 49, 64, 81};
+        int[] square = new int[]{0, 1, 4, 9, 16, 25, 36, 49, 64, 81};
         // Map<Integer, Integer> hash = new HashMap<>();
         
-        
         while(n>9) {
-            n = isHappyHelper(n);
+            n = isHappyHelper(n, square);
         }
         return n == 1 || n == 7;
     }
     
-    private int isHappyHelper(int n) {
-        
+    private int isHappyHelper(int n, int[] square) {
         int sum = 0;
         while(n>0) {
-            // sum += square[n%10];
-            sum += Math.pow(n%10, 2);
+            sum += square[n%10];
             n /= 10;
         }
         return sum;
