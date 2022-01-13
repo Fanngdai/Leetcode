0686aed384449255c0becbbef316be5a496fb7fb
Fanny <17479928+Fanngdai@users.noreply.github.com>
2022-01-13 10:07:47 -0500

    Time: 1 ms (100.00%), Space: 39.7 MB (99.33%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,13 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        // the non-zero values
         int count = 0;
         
+        // loop through array O(n), count stops where the value is 0 until value is replaced
         for(int i=0; i<nums.length; i++) {
             if(nums[i] != 0) {
                 nums[count++] = nums[i];
             }
         }
         
+        // count is now at where the 0's should begin
         for(int i=count; i<nums.length; i++) {
             nums[i] = 0;
         }
