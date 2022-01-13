8f754b173af3e157cebea89ce63284fcd1661540
Fanny <17479928+Fanngdai@users.noreply.github.com>
2022-01-13 14:15:03 -0500

    Time: 0 ms (100.00%), Space: 40.3 MB (40.56%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,9 +1,28 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return searchHelper(nums, target, 0, nums.length-1);
+        // return searchRecursion(nums, target, 0, nums.length-1);
+        return searchNotRecursion(nums, target);
     }
     
-    private int searchHelper(int[] nums, int target, int left, int right) {
+    private int searchNotRecursion(int[] nums, int target) {
+        int left = 0, right = nums.length-1;
+        
+        while(left <= right) {
+            int mid = (left+right) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                // go right
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    private int searchRecursion(int[] nums, int target, int left, int right) {
         if(left > right) {
             return -1;
         }
@@ -12,9 +31,9 @@ class Solution {
         if(nums[mid] == target) {
             return mid;
         } else if(nums[mid] < target) {
-            return searchHelper(nums, target, mid+1, right);
+            return searchRecursion(nums, target, mid+1, right);
         } else {
-            return searchHelper(nums, target, left, mid-1);
+            return searchRecursion(nums, target, left, mid-1);
         }
     }
 }
