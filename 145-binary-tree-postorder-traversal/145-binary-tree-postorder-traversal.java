dbf56945adecae4f1286b6ecabd44c1fe066ad21
Fanny <17479928+Fanngdai@users.noreply.github.com>
2022-01-15 14:50:46 -0500

    Time: 1 ms (26.20%), Space: 39 MB (12.61%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -15,7 +15,38 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        return postorderTraversalRecursion(root);
+        return postorderTraversalStack(root);
+        // return postorderTraversalRecursion(root);
+    }
+    
+    /*
+     * The idea is to get all the way to the leaf
+     * then traverse back up
+     * You want all the subtrees to be added prior to the root
+     */
+    private List<Integer> postorderTraversalStack(TreeNode root) {
+        List<Integer> rtn = new ArrayList<>();
+        if(root == null) return rtn;
+        Stack<TreeNode> stack = new Stack<>();
+        addUntilLeaf(root, stack);
+        
+        while(!stack.isEmpty()) {
+            TreeNode temp = stack.peek();
+            if(root != temp.right) {
+                addUntilLeaf(temp.right, stack);
+            }
+            root = stack.pop();
+            rtn.add(root.val);
+        }
+        
+        return rtn;
+    }
+    
+    private void addUntilLeaf(TreeNode root, Stack<TreeNode> stack) {
+        while(root != null) {
+            stack.push(root);
+            root = root.left == null ? root.right : root.left;
+        }
     }
     
     private List<Integer> postorderTraversalRecursion(TreeNode root) {
@@ -31,3 +62,10 @@ class Solution {
         lst.add(root.val);
     }
 }
+
+/*
+[2,null,3,null,1] -> [1,3,2]
+[1,4,3,null,null,null,2]
+[1,null,4,3,null,null,2]
+[2,null,3,null,1, 4,null, 5, 6,7,8,9,10,1,1] -> bigger tree for the funs
+*/
