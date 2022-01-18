9688683bd79067d598afa8f7299254459b636263
Fanny <17479928+Fanngdai@users.noreply.github.com>
2022-01-18 11:53:04 -0500

    Time: 683 ms (5.12%), Space: 165.1 MB (6.43%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Queue<Integer> minHeap = new PriorityQueue<>();
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] > 0 && !set.contains(nums[i])) {
+                minHeap.add(nums[i]);
+                set.add(nums[i]);
+            }
+        }
+        
+        int rtn = 1;
+        while(!minHeap.isEmpty()) {
+            if(minHeap.poll() != rtn) {
+                return rtn;
+            }
+            rtn++;
+        }
+        
+        return rtn;
+    }
+}
+
+/*
+Logic: put in heap and start counting, we can also sort it.
+I did the min Heap way. While it is not in the heap and is a pos number,
+Add it
+
+[0,2,2,1,1]
+https://leetcode.com/submissions/detail/622618508/testcase/
+*/
