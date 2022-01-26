1e0be23269a2717e6668870c90bdc0fb32f2e96e
Fanny <17479928+Fanngdai@users.noreply.github.com>
2022-01-25 22:32:34 -0500

    Time: 2 ms (70.91%), Space: 38.5 MB (39.72%) - LeetHub

---
+++ 457-circular-array-loop/457-circular-array-loop.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean circularArrayLoop(int[] nums) {
+        int slow = 0, fast = 0, nLen = nums.length;
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<nLen; i++) {
+            nums[i] %= nLen;
+        }
+        
+        for(int i=0; i<nLen; i++) {
+            if(set.contains(i)) continue;
+            set.add(i);
+            if(circularArrayLoopHelper(nums, nLen, set, i)) return true;
+        }
+        
+        return false;
+    }
+    
+    private boolean circularArrayLoopHelper(int[] nums, int nLen, Set<Integer> set, int start) {
+        int slow = start, fast = start;
+        
+        do {
+            slow = (slow + nums[slow] + nLen) % nLen;
+            set.add(slow);
+            fast = (fast + nums[fast] + nLen) % nLen;
+            fast = (fast + nums[fast] + nLen) % nLen;
+        } while(slow != fast);
+        
+        
+        // self loop
+        if((slow + nums[slow] + nLen) % nLen == fast) {
+            return false;
+        }
+        
+        boolean posNeg = nums[slow] > 0;
+        // check for pos or neg
+        do {
+            slow = (slow + nums[slow] + nLen) % nLen;
+            boolean flag = nums[slow] > 0;
+            if(posNeg != flag) {
+                return false;
+            }
+        } while(slow != fast);
+        
+        return true;
+    }
+}
+
+/*
+[3,1,2]
+[-2,-3,-9]
+[2,1,1,-1]
+*/
