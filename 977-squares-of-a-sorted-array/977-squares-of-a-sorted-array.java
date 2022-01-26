f385b5c41ca3ab57e3a768340604d4bf43e391ed
Fanny <17479928+Fanngdai@users.noreply.github.com>
2022-01-25 23:01:15 -0500

    Time: 3 ms (39.69%), Space: 53.3 MB (18.21%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,11 +1,39 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
+        int nLen = nums.length, left = 0, right = nums.length - 1;
+        int[] rtn = new int[nLen];
+        
+        for(int i=nLen-1; i>=0; i--) {
+            if(nums[right] > nums[left] * -1) {
+                rtn[i] = (int)Math.pow(nums[right--], 2);
+            } else {
+                rtn[i] = (int)Math.pow(nums[left++], 2);
+            }
+        }
+        
+        return rtn;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public int[] sortedSquaresMyWay(int[] nums) {
         for(int i=0; i<nums.length; i++) {
             nums[i] *= nums[i];
         }
-        
         Arrays.sort(nums);
-        
         return nums;
     }
 }
+
+/*
+[-4,-1,0,3,10]
+[-7,-3,2,3,11]
+[-5,-3,-2,-1]
+[-5,-3,1,2]
+[1]
+[-1]
+*/
