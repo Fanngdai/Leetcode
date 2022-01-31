01e47f4ee58ab6b4e663a911b2908b293073ff60
Fanny <17479928+Fanngdai@users.noreply.github.com>
2022-01-31 16:55:49 -0500

    Time: 13 ms (12.42%), Space: 59.2 MB (5.32%) - LeetHub

---
+++ 690-employee-importance/690-employee-importance.java
@@ -0,0 +1,39 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        // for easy employee access
+        HashMap<Integer, Employee> hashMap = new HashMap<>();
+        for(Employee e: employees) {
+            hashMap.put(e.id, e);
+        }
+        
+        // for dealing with calc importance
+        Queue<Employee> queue = new LinkedList<>();
+        queue.add(hashMap.get(id));
+        hashMap.remove(id);
+        
+        int rtn = 0;
+        
+        while(!queue.isEmpty()) {
+            Employee currEmployee = queue.poll();
+            rtn += currEmployee.importance;
+            
+            for(int i: currEmployee.subordinates) {
+                if(hashMap.containsKey(i)) {
+                    queue.add(hashMap.get(i));
+                    hashMap.remove(i);
+                }
+            }
+        }
+        
+        return rtn;
+    }
+}
