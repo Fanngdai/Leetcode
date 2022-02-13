5fab347e6bfbe970513aef292abd07cca84d6824
Fanny <17479928+Fanngdai@users.noreply.github.com>
2022-02-12 22:03:11 -0500

    Time: 9 ms (56.76%), Space: 45.3 MB (11.65%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.java
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -3,10 +3,12 @@ class Solution {
         HashMap<String, Integer> counter = new HashMap<>();
         int wLen = words.length;
         
+        // put into hashmap, count how many words
         for(int i=0; i<wLen; i++) {
             counter.put(words[i], counter.getOrDefault(words[i], 0) + 1);
         }
         
+        // max heap, if amt is the same, compare string
         PriorityQueue<Pair<String, Integer>> heap = new PriorityQueue<>(
             (a, b) -> b.getValue() - a.getValue() != 0 ? b.getValue() - a.getValue() : a.getKey().compareTo(b.getKey()));
         
@@ -16,6 +18,7 @@ class Solution {
             heap.add(pair);
         }
         
+        // get the top k amt and put in return lst.
         List<String> rtn = new ArrayList<>();
         for(int i=0; i<k; i++) {
             rtn.add(heap.poll().getKey());    
